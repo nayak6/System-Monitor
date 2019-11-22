bc7487d4b10c2df40b78ffc7cc72f445462d0697
Nikhil Narendra Iyer <fnikhil@moore15.cs.purdue.edu>
2019-11-22 16:42:56 -0500

    Iyer - open cpuinfo, read from file and display in listwidget

--- SystemMonitor/mainwindow.cpp
+++ SystemMonitor/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,15 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->listWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    ui->listWidget->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+    QFile file("/proc/cpuinfo");
+    if(!file.open(QIODevice::ReadOnly))
+        QMessageBox::information(0, "info", file.errorString());
+    QTextStream in(&file);
+    ui->listWidget->addItem(in.readAll());
 }
